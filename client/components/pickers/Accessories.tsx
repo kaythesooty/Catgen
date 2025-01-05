a06c1533e582101330b34ae312b1785e3621367c
Riley Wessel <lessewyendor@gmail.com>
2025-01-05 13:56:25 +1300

    rework collar picker

--- client/components/pickers/Accessories.tsx
+++ client/components/pickers/Accessories.tsx
@@ -15,25 +15,30 @@ export function Accessories({ setter, cat }: Props) {
   const { code: herbValues, eng: herbNames } = accessories.herb
   const { code: wildValues, eng: wildNames } = accessories.wild
 
+  const setCollar = (collarName: string) => {
+    setCollartype(collarName)
+    setter({...cat, accessoryType: collarName})
+  }
+
   return (
   <div className="picker">
     <h3>COLLARS</h3>
     <label>Collar type:
-      <select value={collartype} onChange={(e) => setCollartype(e.target.value)}>
+      <select value={collartype} onChange={(e) => setCollar(e.target.value)}>
         {collarTypes.map((opt, i) => <option value={opt} key={i}>{collarNames[i]}</option>)}
       </select>
     </label>
     {colourValues.map((clr, i) => <button
       key={i}
-      onClick={() => setter({...cat, accessory: `${clr}${collartype}`})}>
+      onClick={() => setter({...cat, accessoryType: `${clr}${collartype}`})}>
       {colourNames[i]}
     </button>)}
     <div className="line"></div>
     <h3>HERBS</h3>
-    {herbValues.map((hrb, i) => <button key={i} onClick={() => setter({...cat, accessory: hrb})}>{herbNames[i]}</button>)}
+    {herbValues.map((hrb, i) => <button key={i} onClick={() => setter({...cat, accessoryType: hrb})}>{herbNames[i]}</button>)}
     <div className="line"></div>
     <h3>WILD</h3>
-    {wildValues.map((wld, i) => <button key={i} onClick={() => setter({...cat, accessory: wld})}>{wildNames[i]}</button>)}
+    {wildValues.map((wld, i) => <button key={i} onClick={() => setter({...cat, accessoryType: wld})}>{wildNames[i]}</button>)}
   </div>
   )
 }
