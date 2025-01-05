88597c1bdf6f979142e22b71686ffb52ecb14c81
Riley Wessel <lessewyendor@gmail.com>
2025-01-05 18:08:07 +1300

    add vitiligo and point markings to white patch picker

--- client/components/pickers/White.tsx
+++ client/components/pickers/White.tsx
@@ -9,10 +9,40 @@ interface Props {
 export function White({ setter, cat }: Props) {
   return (
     <div className="picker">
+      <h3>WHITE PATCHES</h3>
       <button onClick={() => setter({ ...cat, white_patches: null })}>NONE</button>
-      {whitePatches.masterList.map((ptn, i) => (
+      <h4>LOW WHITE</h4>
+      {whitePatches.lowWhite.code.map((ptn, i) => (
         <button key={i} onClick={() => setter({ ...cat, white_patches: ptn })}>
-          {ptn}
+          {whitePatches.lowWhite.eng[i]}
+        </button>
+      ))}
+      <h4>MID WHITE</h4>
+      {whitePatches.midWhite.code.map((ptn, i) => (
+        <button key={i} onClick={() => setter({ ...cat, white_patches: ptn })}>
+          {whitePatches.midWhite.eng[i]}
+        </button>
+      ))}
+      <h4>HIGH WHITE</h4>
+      {whitePatches.highWhite.code.map((ptn, i) => (
+        <button key={i} onClick={() => setter({ ...cat, white_patches: ptn })}>
+          {whitePatches.highWhite.eng[i]}
+        </button>
+      ))}
+      <div className="line"></div>
+      <h3>VITILIGO</h3>
+      <button onClick={() => setter({ ...cat, vitiligo: null })}>NONE</button>
+      {whitePatches.vitiligo.code.map((ptn, i) => (
+        <button key={i} onClick={() => setter({ ...cat, vitiligo: ptn })}>
+          {whitePatches.vitiligo.eng[i]}
+        </button>
+      ))}
+      <div className="line"></div>
+      <h3>POINT MARKINGS</h3>
+      <button onClick={() => setter({ ...cat, point: null })}>NONE</button>
+      {whitePatches.points.code.map((ptn, i) => (
+        <button key={i} onClick={() => setter({ ...cat, point: ptn })}>
+          {whitePatches.points.eng[i]}
         </button>
       ))}
     </div>
